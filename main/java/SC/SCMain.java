aa710257811b0b9573580293856f5b02c85a5c6c
Ross McTaggart <badtigra@protonmail.com>
2024-06-13 13:08:32 -0400

    Added comments

--- main/java/SC/SCMain.java
+++ main/java/SC/SCMain.java
@@ -1,9 +1,8 @@
 /**
  * Program to make a seating chart and add names
- * Drives SCChild
  *
  * @author Gregory Ross McTaggart
- * @version June 12, 2024
+ * @version June 13, 2024
  */
 package SC;
 
@@ -29,14 +28,17 @@ public class SCMain {
         boolean validData = false;
         boolean notZero = false;
 
+        // Introduces program's placeholder name
         System.out.printf("Welcome to Express Seat Charter%n%n");
 
         do {
             try {
                 System.out.printf("How many rows and columns? (Format: \"X X\"): ");
-                numRows = input.nextInt(); // Need handling in case 0 is entered
-                numColumns = input.nextInt(); // Need handling in case 0 is entered
+                numRows = input.nextInt();
+                numColumns = input.nextInt();
 
+                // While Loop used to ensure that values only greater than 0 are
+                // accepted into the program 
                 while (!notZero) {
                     if (numRows > 0 && numColumns > 0) {
                         notZero = true;
@@ -44,15 +46,21 @@ public class SCMain {
                         System.out.printf("Please enter values greater than zero.%n%n");
                         break;
                     }
+                    // Pass While Loop when conditions are met
 
                     System.out.print("Please type 'exit' to close program. Name? ");
                     input.nextLine();
                     name = input.nextLine();
 
+                    // Initializes custom 2D array with inputted values
+                    // And reminds user of what they entered
                     SCChild sc = new SCChild(numRows, numColumns);
                     System.out.printf("%nThere are %d rows and %d columns available%n",
                             sc.getRows(), sc.getColumns());
 
+                    // Core loop of program
+                    // While Loop used to continually 
+                    // add names to elements in the 2D array
                     while (!(name.equals("exit") || name.equals("EXIT"))) {
                         try {
                             System.out.print("Row & Column? ");
@@ -63,8 +71,8 @@ public class SCMain {
                             sc.addPerson(row, column, name);
 
                             System.out.print("Name? ");
-
-                            input.nextLine();
+                            
+                            input.nextLine(); // To clear the input buffer 
                             name = input.nextLine();
 
                             System.out.println();
@@ -80,19 +88,27 @@ public class SCMain {
 
                         }
                     }
+                    // Boolean to prevent further looping
                     validData = true;
+                    // Chart generation block
                     System.out.printf("%n\tChart Begin%n%n");
                     sc.printSeatingChart();
                     System.out.printf("%n%n\tChart End%n");
+                    // Tells user how many chairs are occupied and how many 
+                    // are unoccupied
                     System.out.printf("%nYou have %d assigned chairs and %d empty chairs%n",
                             sc.getAssigned(), sc.getEmpty());
+                    // Prints name in alphabetical order, starting with first
+                    // character
                     sc.printNames();
+                    // Prints names in order of first 
+                    // element to last element
                     sc.printNamesNumeric();
                 }
             } catch (InputMismatchException ie) {
                 System.err.printf("%nThat is not a valid input.%n"
-                        + "Please enter an integer in the format \"X X\".%n");
-                input.next();
+                        + "Please enter an integer in the format \"X X\".%n%n");
+                input.nextLine(); // To clear the input buffer
             }
         } while (!validData);
         input.close();
